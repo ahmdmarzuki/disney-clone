8016dab9376fe309fdb7c34e93783123f833e60f
ahmdmarzuki <ahmadmarzuki303@gmail.com>
2025-03-26 15:43:50 +0700

    NavBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import logo from "./../assets/images/logo.png";
+import {
+  HiHome,
+  HiMagnifyingGlass,
+  HiStar,
+  HiPlayCircle,
+  HiTv,
+} from "react-icons/hi2";
+import { HiPlus, HiDotsVertical } from "react-icons/hi";
+import HeaderItem from "./HeaderItem";
 
 const Header = () => {
-  return <div>Header</div>;
+  const menu = [
+    {
+      name: "HOME",
+      icon: HiHome,
+    },
+    {
+      name: "SEARCH",
+      icon: HiMagnifyingGlass,
+    },
+    {
+      name: "WATCH LIST",
+      icon: HiPlus,
+    },
+    {
+      name: "ORIGINALS",
+      icon: HiStar,
+    },
+    {
+      name: "MOVIES",
+      icon: HiPlayCircle,
+    },
+    {
+      name: "SERIES",
+      icon: HiTv,
+    },
+  ];
+  return (
+    <div className="flex items-center justify-between w-screen">
+      <div className="flex gap-8 items-center">
+        <img src={logo} className="w-[80px] md:w-[115px] object-cover" />
+        {menu.map((item) => (
+          <HeaderItem title={item.name} Icon={item.icon} />
+        ))}
+      </div>
+      <img
+        src="https://images.unsplash.com/photo-1742576948659-3c630862a38d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8g"
+        className="w-[40px] h-[40px] rounded-full"
+      />
+    </div>
+  );
 };
 
 export default Header;
